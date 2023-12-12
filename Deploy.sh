0a4a6a6001f0400025e02fa395be760e53dfeed2
CSK <karunakarthokala07@gmail.com>
2023-12-12 19:21:21 +0530

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,3 +1,3 @@
 This is also great contains data and code.ok good devolvper
 dont know
-This code is updated/Modified by Developer Srininvas....
+This code is updated/Modified by Developer Srininvas....ok sir .code has been modified
